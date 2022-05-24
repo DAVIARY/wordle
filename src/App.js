125bac3cf94501608c79f19356949a727636d8ba
DAVIARY <daviary12@gmail.com>
2022-05-24 04:57:42 -0400

    install router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import WordleContainer from './components/WordleContainer'
-
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import DrawBoard from './components/Board';
+import Keyboard from './components/Keyboard';
 
 const App = () =>{
   return(
-    
+    <Router>
       <WordleContainer />
-    
+    <Route path='/' component={DrawBoard} ></Route>  
+    </Router>
   )
 }
 
